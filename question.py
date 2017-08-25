a20d5eb4aafbb2052bae15a690d49dd1285bcd03
Toby Hunag <619058900@qq.com>
2017-08-25 15:08:01 +1000

    try somthing, there are some bug to be fix

--- question.py
+++ question.py
@@ -7,19 +7,58 @@ class question():
     # a dict to store all the instance of this object
     _questDisct = {}
 
+    def __str__(slef):
+        # print this class
+        string = ""
+        if slef.id:
+            string = str(self.id )+ str(self.question)+str(self.answers)
+            return string
+        return "this is a tree object"
+
+
     def get_csvName():
         return _csv_fileName;
 
     def __init__(self, row= None):
         super(question, self).__init__()
+        if row != None:
+            # initialise for node object
+            self.id = int(row[0])
+            self.question =str(row[1])
+            self.answers = row[2:] # grep all the things after three
 
-    def csv_readRow(self,row):
+            # assing this item into the diction to prepare searching
+            _questDisct[self.id]= self
+        else:
+            # this obj act like a tree object
+
+            pass
+
+    def csv_readRow(row):
         # try to read each row in this function
         # after reading a row add an instance of this class into the _questDisct
         # data_struct is: id, question, [answers]..
-        pass
 
+        # create the new item to append
+        new =  question(row)
 
+    def csv_writeRow():
+        return_array =[]
+        for key,values in _questDisct:
+            return
+        return return_array
+
+    def find_question(ques_id = None):
+        if ques_id == None:
+            # no input values, return all the values back
+            return _questDisct.values()
+        else :
+            # input some things
+            return_list = []
+            for i in ques_id:
+                # searching all the given values
+                return_list.append(_questDisct[i] )
+            return return_list
 
 class myCSV():
     """docstring for myCSV."""
@@ -29,7 +68,7 @@ class myCSV():
     def write(obj):
         with open(obj.get_csvName(),'a') as csv_out:
             writer = csv.writer(csv_out)
-            writer.writerow([name, zID, desc])
+            writer.writerow(obj.csv_writeRow())
 
     def read(obj):
         with open(obj.get_csvName(),'r') as csv_in:
@@ -44,3 +83,14 @@ class myCSV():
 if __name__ == '__main__':
     # unittests
     myCSV.write(question);
+    # this_survey = survey('1511-17s1')
+    # 1.q1
+    # 2.q2
+    # 3.q3
+    # [
+    #     [1,q1,answers]
+    #     [2,q2,answers]
+    # ]
+    # this_survey.add_question()
+
+    question.find_question([1,2])
