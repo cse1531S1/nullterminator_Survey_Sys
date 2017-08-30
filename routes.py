ce53f9493538736746df8be4b3b0cbe898241eba
Toby Huang <619058900@qq.com>
2017-08-30 23:51:10 +1000

    tidy the unneccessary code

--- routes.py
+++ routes.py
@@ -16,31 +16,21 @@ def course():
     return courselist
 
 def question():
-    question_list=[]
-    question_number0=1
-    question0="what is your name?"
-    single_question0=[question_number0,question0,"A. hugo","B. ruofei","C.daniel","D.hendri"]
-
-    question_number1=2
-    question1="what is your favorite fruit?"
-    single_question1=[question_number1,question1,"A. apple","B. banana","C. rockmelon"]
-
-    question_number2=3
-    question2="what is your favorite color?"
-    single_question2=[question_number2,question2,"A. red","B. blue"]
-
-    question_list.append(single_question0)
-    question_list.append(single_question1)
-    question_list.append(single_question2)
-    print(question_list)
+    # test caset question list
+    question_list=[
+        [0,"what is your name?","A. hugo","B. ruofei","C.daniel","D.hendri"],
+        [1,"what is your favorite fruit?","A. apple","B. banana","C. rockmelon"],
+        [2,"what is your favorite color?","A. red","B. blue"]
+    ]
+    # print(question_list)
     return question_list
 
 def list_number_of_answer(question_list):
-    list_number_of_answer=[]
-    for i in range(len(question_list)):
-        list_number_of_answer.append(len(question_list[i])-2)
-    #print(list_number_of_answer)
-    return list_number_of_answer
+    answer_number_list=[]
+    for i in question_list:
+        answer_number_list.append(len(i)-2)
+    #print(answer_number_list)
+    return answer_number_list
 
 def storedquestion(qid=[],coursename=""):
     print(qid)
@@ -51,8 +41,8 @@ def storedquestion(qid=[],coursename=""):
             for i in range(len(all_question)):
                 if (i+1) == int(qid[j]):
                     print(all_question[i])
-                    writer.writerow(all_question[i]) 
-    
+                    writer.writerow(all_question[i])
+
 @app.route("/coursepage/<string:coursename>/studentsurvey", methods=["GET", "POST"])
 def student_part(coursename=""):
     with open('%s.csv' % coursename,'r') as csv_in:
@@ -70,19 +60,18 @@ def finalsurvey(coursename):
         for row in reader:
             questionlist.append(row)
         print(questionlist)
-    return render_template("finalsurvey.html", course_name=coursename, questionfield=questionlist,length=len(questionlist), number_of_answer=list_number_of_answer(questionlist) )      
+    return render_template("finalsurvey.html", course_name=coursename, questionfield=questionlist,length=len(questionlist), number_of_answer=list_number_of_answer(questionlist) )
 
 @app.route("/coursepage/<string:coursename>", methods=["GET", "POST"])
 def coursepage(coursename):
     if request.method == "POST":
-        selected_q = request.form.getlist("check_list[]") 
+        selected_q = request.form.getlist("check_list[]")
         storedquestion(selected_q,coursename) #create a csv file
         return redirect(url_for('finalsurvey',coursename=coursename))
     return render_template("surveycreate.html", course_name=coursename, questionfield=question(),length=len(question()), number_of_answer=list_number_of_answer(question()) )
 
 @app.route("/selectcourse", methods=["GET", "POST"])
 def course_adding():
-   
     if request.method == "POST":
         return redirect(url_for('coursepage', coursename=request.form["co"]))
     return render_template("courselect.html", course=course(), length=len(course()) )
