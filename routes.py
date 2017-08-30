bafaeee8b2cd852f52b8bf4701fe9794fc3055d6
hugo <z5084353@ad.unsw.edu.au>
2017-08-30 14:41:08 +1000

    add finalsurvey part and it could generate selected question csv file

--- routes.py
+++ routes.py
@@ -43,15 +43,42 @@ def list_number_of_answer(question_list):
         list_number_of_answer.append(len(question_list[i][2]))
     print(list_number_of_answer)
     return list_number_of_answer
+ 
+
+def storedquestion(qid=[],coursename=""):
+    print(qid)
+    with open('%s.csv' % coursename,'w+') as csv_out:
+        writer = csv.writer(csv_out)
+        all_question = question()
+        for j in range(len(qid)):
+            for i in range(len(all_question)):
+                if (i+1) == int(qid[j]):
+                    print(all_question[i])
+                    writer.writerow(all_question[i]) 
     
+@app.route("/coursepage/<string:coursename>/finalsurvey", methods=["GET", "POST"])
+def finalsurvey(coursename):
+    with open('%s.csv'% coursename,'r') as csv_in:
+        reader = csv.reader(csv_in)
+        questionlist=[]
+        for row in reader:
+            questionlist.append(row)
+        print(questionlist)
+    return render_template("finalsurvey.html", course_name=coursename, questionfield=questionlist,length=len(questionlist), number_of_answer=list_number_of_answer(questionlist) )      
+
+
+
 
 
 @app.route("/coursepage/<string:coursename>", methods=["GET", "POST"])
 def coursepage(coursename):
     if request.method == "POST":
-        selected_q = request.form.getlist("check_list[]")
-        print(selected_q)
+        selected_q = request.form.getlist("check_list[]") 
+        storedquestion(selected_q,coursename) #create a csv file
+        return redirect(url_for('finalsurvey',coursename=coursename))
     return render_template("surveycreate.html", course_name=coursename, questionfield=question(),length=len(question()), number_of_answer=list_number_of_answer(question()) )
+
+
 
 
 
