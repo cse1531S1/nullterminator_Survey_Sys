71ed950d908f2c5107036635c5cd532175517681
Toby Hunag <619058900@qq.com>
2017-09-01 10:23:13 +1000

    fix some small bugs

--- routes.py
+++ routes.py
@@ -18,9 +18,9 @@ def add_question():
         if add_q.is_valid_Q(question,answers) == 0:
             # valid input
             # push the user input into system
-            aqq_q.add_Q(question, answers)
+            add_q.add_Q(question, answers)
             # get back the successful page to user
             return render_template("success_add_q.html",add_more = url_for("add_question"))
         # invalid input, push back what user has been input, and push the error message
-        return render_template("add_q.html",request = url_for("addQuestion"),error = add_q.is_valid_Q(question,answers),question = question, answers = request.form["answers"])
-    return render_template("add_q.html",request = url_for("addQuestion"))
+        return render_template("add_q.html",request = url_for("add_question"),error = add_q.is_valid_Q(question,answers),question = question, answers = request.form["answers"])
+    return render_template("add_q.html",request = url_for("add_question"))
