2874b824f6b5e55b2d205c136a0c5b29f8f92c77
Daniel <z5162121@unsw.edu.au>
2017-09-04 14:44:01 +1000

    Skeleton of object oriented structure for the survey classes

--- survey.py
+++ survey.py
@@ -1,3 +1,5 @@
+import question
+
 class survey:
 
 	def __init__(self, s_id, questions):
@@ -6,7 +8,25 @@ class survey:
 		#Survey ID will dictate a file which will be read from
 		#questions contains a list of questions will be read from the surveys csv		
 
-#A surveys Response response of a survey to a file
+
+#Class representing a partitcular courses survey structure
+class course:
+
+	def __init__(self, course_id, q_list):
+		self.course_id = course_id
+		self.q_list = q_list
+		
+	def show_survey(self,course_id):
+		#from routes.py file already...
+		
+	def publish_survey(self):
+		#Publish a link to the survey, show the link on the screen	
+	
+	
+	
+	
+	
+#Class representing a response from a respondent
 class response:
 	#						      Question       1,2,3,4,5
 	#response is a list of answers e.g. -> [s_id A,B,C,C,D]
@@ -15,18 +35,20 @@ class response:
 		self.s_id = s_id
 		
 	#returns a list containing values for the new response list	
-	def get_response():
+	def get_response(self):
 	
 		new_resp = #...get the response from the survey page after pressing submit	
 		return new_resp
-		
-	 			
+		 			
 	#Writes the response of a survey to a data csv file	
+	#response as a list of answers, with each index representing a question id
 	def write_response(self, s_id, response):
 		name = s_id + ".data.csv"
 		response = get_response()
 		with open(name, "a") as f:
 		writer = csv.writer(f)
-		writer.writerows(response)		
+		writer.writerows(response)	
+		
+		
 	
 
