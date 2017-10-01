e3e63d45b8dcb3ad32d88e2652578af09ca1d19e
Daniel <z5162121@student.unsw.edu.au>
2017-10-01 22:01:30 +1100

    Authentication module works

--- authenticate.py
+++ authenticate.py
@@ -1,9 +1,9 @@
 from flask import Flask, redirect, request, render_template, url_for
 from flask_login import LoginManager,login_user,current_user,login_required,logout_user
-from user import User
-from server import app,login_manager
+#from user import User
+from server import app,login_manager,get_db
 import sqlite3
-from db.sql_uti import SqlUtil
+from sql_uti import SqlUtil
 from werkzeug.security import check_password_hash,generate_password_hash
 import csv
 
@@ -11,89 +11,84 @@ import csv
 def check_password(user_id, password):
     valid = compare_password(user_id, password)
     if valid == True:
-
-        user = User(user_id)
-        login_user(user)
-
         return True
     return False
 
 #Get information about the user from the database
-def get_user(user_id):
-    new_user = User()
-    #User.name(user_id) = get_name(user_id)
-    new_user.role = get_role(user_id)
-
-    return new_user
+#def get_user(user_id):
+#    new_user = User()
+#    new_user.role = get_role(user_id)
+#    return new_user
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    user = get_user(user_id)
-    return user
+#@login_manager.user_loader
+#def load_user(user_id):
+#    user = get_user(user_id)
+#    return user
 
 
 def compare_password(user_id, password):
 
-    #Query Using sql_util ...
-
-    #connection = sqlite3.connect('user')  #TABLE FOR USER ????
-    #c = conn.cursor()
-    #c.execute("SELECT * FROM user WHERE id =?", (user_id))
-    #con.commit()
-    #row = cur.fetchone()
     user = SqlUtil("users")
     row = user.find("id",user_id).one()
-    #print(row)
-    return check_password_hash(password, row[1])
+    hash_pass = row[1]
+    return check_password_hash(hash_pass, password)
 
-def get_role(user_id):
+#def get_role(user_id):
 
-    #Query Using sql_util ...
+#    user = SqlUtil("users")
+#    row = user.find("id",user_id).one()
+#    role = row[2]
 
-    #connection = sqlite3.connect('user')  #TABLE FOR USER ????
-    #c = conn.cursor()
-    #c.execute("SELECT * FROM user WHERE id =?", (user_id))
-    #con.commit()
-    #row = cur.fetchone()
-    user = SqlUtil("users")
-    row = user.find("id",user_id).one()
-    role = row[2]
-
-    return role
-
-
-
+#    return role
 
 
 #test authentication
 if __name__ == '__main__':
 #    user = SqlUtil("users")
+    with app.app_context():
 
-    print('=== Test Hashing comparison ===')
-    password = 'secret_password'
-    print ("Raw Password: " +  password)
-    hash_pass = generate_password_hash(password)
-    print ("Hash password: " + hash_pass)
-    isvalid = check_password_hash(hash_pass, password)
-    print(isvalid)
+        user = SqlUtil("users")
 
-    password = 'secret_password'
-    print ("Raw Password: " +  password)
-    hash_pass = generate_password_hash(password)
-    print ("Hash password: " + hash_pass)
-    password = 'fake_secret'
-    isvalid = check_password_hash(hash_pass, password)
-    print(isvalid)
-    print ("==================")
+        print('=== Test Hashing comparison ===')
+        password = 'secret_password'
+        print ("Raw Password: " +  password)
+        hash_pass = generate_password_hash(password)
+        print ("Hash password: " + hash_pass)
+        isvalid = check_password_hash(hash_pass, password)
+        print(isvalid)
 
-    print("=== Test on known user 50 - valid ===")
+        password = 'Correct_password'
+        print ("Raw Password: " +  password)
+        hash_pass = generate_password_hash(password)
+        print ("Hash password: " + hash_pass)
+        password = 'wrong_password'
+        isvalid = check_password_hash(hash_pass, password)
+        print(isvalid)
 
-    isValid = compare_password(50, 'staff670')
-    print('Password ' + isValid)
+        print ("==================")
+        print("\n\nInserting user into db:")
 
-    print("=== Test on known user 50 - invalid ===")
+        hash_pass = generate_password_hash("daniel")
+        user.insert(["id","password","role"],[123411111111,hash_pass,"test"]).save()
+        print(user.find("id",123411111111).one())
 
-    isValid = compare_password(50, 'staffasd70')
-    print('password' + isValid)
+        print("=== Test on known user - valid ===")
+        row = user.find("id",123411111111).one()
+        print("Compare on correct password")
+        isValid = check_password(123411111111,"daniel")
+        print(isValid)
+
+        print("\n=== Test on known user - invalid ===")
+
+        row = user.find("id",123411111111).one()
+        print("Compare on correct password")
+        isValid = check_password(123411111111,"touyhjby")
+        print(isValid)
+
+
+        print("\nDelete the inserted item, this sql would execute:")
+        user.find("id",123411111111).test_exe().delete()
+        print("\nTest whether user 1 have been deleted:")
+        print(user.find("id",123411111111).all())
     #################################
