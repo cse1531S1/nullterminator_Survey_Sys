1fc52c2981984c5b948c2f5670c437fd2472e97d
Hendri Chua <hendrisgia0004@gmail.com>
2017-10-07 16:56:27 +1100

    edited respond table and add some functionality

--- new_respond.py
+++ new_respond.py
@@ -5,55 +5,29 @@ class Respond(SqlUtil):
     """docstring for respond."""
     def __init__(self):
         super().__init__("respond")
-    def find_by_surveyID(self, survey_id):
-        if not type(survey_id) in [int,str]:
-            raise TypeError("ID from survey must be a integer")
-        # simple case of mapping a function to dynamic execute SQL
-        return self.find("id",int(survey_id)).one()
-    def insert_response(self,survey_id,uid):
-        return self.insert(["survey_id","uid"],[survey_id,uid]).save()
-    def get_respond(self,survey_id):
-        this_respond = self.find("survey_id",survey_id)\
-                        .find("uid",uid).one()
-        return this_course()
-    def delete_respond(self,id):
-        if type(id)!= int:
-            raise TypeError("input id must be int")
+    
+    def get_response_course_qid(self,course_id,q_id):
+        return self.find("course_id",course_id).find("q_id",q_id).all()
+    
+    def get_all_response_by_course(self,course_id):
+        return self.find("course_id",course_id).all()
+    
+    def insert_response(self,course_id,q_id,answer):
+        return self.insert(["course_id","q_id","answer"],[course_id,q_id,answer]).save()
+
+    def delete_response_by_id(self,id):
         self.find("id",id).delete()
 
-class RespondMCQ(SqlUtil):
-    """docstring for res_mcq"""
-    def __init__(self):
-        super().__init__("res_mcq")
-    def find_by_resID(self,respond_id):
-        if not type(respond_id) in [int,str]:
-            raise TypeError("ID from respond must be a integer")
-        # simple case of mapping a function to dynamic execute SQL
-        return self.find("id",int(respond_id)).one()
-    def insert_res_mcq(self,respond_id,ans_list):
-        return self.insert("respond_id",respond_id).insert("ans_list","&&".join(ans_list)).save()
-        
-class RespondText(SqlUtil):
-    """docstring for res_text"""
-    def __init__(self):
-        super().__init__("res_text")
-    def find_by_resID(self,respond_id):
-        if not type(respond_id) in [int,str]:
-            raise TypeError("ID from respond must be a integer")
-        # simple case of mapping a function to dynamic execute SQL
-        return self.find("id",int(respond_id)).one()
-    def insert_res_text(self,respond_id,ques_id,answer):
-        return self.insert(["respond_id","ques_id","answer"],[respond_id,ques_id,answer]).save()
-        
-class RespondController():
-    def __init__(self):
-        self.__respond = Respond()
-        self.__res_mcq = RespondMCQ()
-        self.__res_text = RespondText()
-        
+    def delete_all_response_by_course(self,course_id):
+        self.find("course_id",course_id).delete()
+
 if __name__ == '__main__':
     res = Respond()
-    res.test_exe().insert_response(1,50)
-    print(res.get_respond)
-    res.delete_respond(1)
-    print(res.get_respond)
+    res.insert_response(1,1,"abc")
+    print(res.get_response_course_qid(1,1))
+    res.delete_response_by_id(1)
+    res.insert_response(1,1,"bcd")
+    res.insert_response(1,1,"cde")
+    print(res.get_all_response_by_course(1))
+    res.delete_all_response_by_course(1)
+    print(res.get_all_response_by_course(1))
